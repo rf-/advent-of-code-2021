0fa5808df84e0c7182f838005b4c30a6f9657075
Ryan Fitzgerald <rwfitzge@gmail.com>
2021-11-30 21:41:34 -0800

    Day 1

---
+++ day1.rb
@@ -0,0 +1,9 @@
+input = File.readlines('day1.input').map(&:to_i)
+
+result_1 = input.each_cons(2).count { |a, b| a < b }
+
+puts result_1
+
+result_2 = input.each_cons(3).each_cons(2).count { |a, b| a.sum < b.sum }
+
+puts result_2
