e538c20c633132898d12ae62f1d188c889ca6ceb
Ryan Fitzgerald <rwfitzge@gmail.com>
2021-12-02 22:00:27 -0800

    Day 3 cleanup

--- day3.rb
+++ day3.rb
@@ -16,25 +16,21 @@ end
 
 bits_by_place = input.map { _1.split("") }.transpose
 
-gamma = bits_by_place.map(&most_common).join("").to_i(2)
-epsilon = bits_by_place.map(&least_common).join("").to_i(2)
+gamma, epsilon = [most_common, least_common].map do |rule|
+  bits_by_place.map(&rule).join("").to_i(2)
+end
 
 puts gamma * epsilon # 2583164
 
 # Part 2
 
-values = input
-oxygen_rating = (0..).each do |place|
-  wanted_bit = most_common.(values.map { _1[place] })
-  values = values.filter { _1[place] == wanted_bit }
-  break values[0].to_i(2) if values.size == 1
+oxygen, co2 = [most_common, least_common].map do |rule|
+  values = input
+  (0..).each do |place|
+    wanted_bit = rule.(values.map { _1[place] })
+    values = values.filter { _1[place] == wanted_bit }
+    break values[0].to_i(2) if values.size == 1
+  end
 end
 
-values = input
-scrubber_rating = (0..).each do |place|
-  wanted_bit = least_common.(values.map { _1[place] })
-  values = values.filter { _1[place] == wanted_bit }
-  break values[0].to_i(2) if values.size == 1
-end
-
-puts oxygen_rating * scrubber_rating # 2784375
+puts oxygen * co2 # 2784375
